b058eab1c99aa4fa0264ba58a83fa1b0a5d11e08
unknown <bhargav-nunna>
2018-01-03 02:41:00 -0500

    Added API for Light on and Light off

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
 //Load HTTP Module
 var http = require ("http");
+// Require express and create an instance of it
+var express = require('express');
+var app = express();
 
-//Create HTTP Serber and listen on port 2314 for requests
-http.createServer(function (req, res) {
-	res.writeHead(200,{'Content-Type': 'text/plain'});
+// on the request to root (localhost:3000/)
+app.get('/', function (req, res) {
+    res.send('<b>My</b> first express http server');
+});
 
-	res.end('Hello World\n');
-}).listen(2314);
+// On localhost:2314/LightOn
+app.get('/LightOn', function (req, res) {
+    
+    https.get('http://192.168.1.28/Relay=ON', (resp) => {
+    	let data = '';
+    	// A chunk of data has been recieved.
+    	resp.on('data', (chunk) => {
+    		data += chunk;
+    	});
+    	// The whole response has been received. Print out the result.
+    	resp.on('end', () => {
+    		console.log(JSON.parse(data).explanation);
+    	});
+    }).on("error", (err) => {
+    	console.log("Error: " + err.message);
+    });
 
-console.log('Server running on port 2314')
+    res.send('OK');
+});
 
+// On localhost:2314/LightOff
+app.get('/LightOff', function (req, res) {
+    
+    https.get('http://192.168.1.28/Relay=OFF', (resp) => {
+    	let data = '';
+    	// A chunk of data has been recieved.
+    	resp.on('data', (chunk) => {
+    		data += chunk;
+    	});
+    	// The whole response has been received. Print out the result.
+    	resp.on('end', () => {
+    		console.log(JSON.parse(data).explanation);
+    	});
+    }).on("error", (err) => {
+    	console.log("Error: " + err.message);
+    });
+
+    res.send('OK');
+});
+
+// Change the 404 message modifing the middleware
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
+});
+
+// start the server in the port 2314 !
+app.listen(2314, function () {
+    console.log('Example app listening on port 2314.');
+});
